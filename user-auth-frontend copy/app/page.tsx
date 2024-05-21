3b884be6f5f7360fcc9a667bec8d28db6759baed
dsaffer <56101196+dsaffer@users.noreply.github.com>
2024-05-20 20:08:57 -0400

    C:/Program Files/Git/ to home

--- user-auth-frontend copy/app/page.tsx
+++ user-auth-frontend copy/app/page.tsx
@@ -1,8 +1,27 @@
 'use client';
 
-const MainPage: React.FC = () => {
-  return null; // This page should never be shown because of the middleware
+import React from 'react';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+
+const HomePage: React.FC = () => {
+    const router = useRouter();
+
+    const handleLogout = () => {
+        // Remove the JWT cookie
+        Cookies.remove('jwt');
+        
+        // Redirect to login page or refresh home page to show logged-out state
+        router.push('/auth');
+    };
+
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>You are logged in.</p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    );
 };
 
-export default MainPage;
-
+export default HomePage;
